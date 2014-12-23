d160bcba54818d57305d4222b9f45267d2beed46
Thomas Meagher <thom.meagher@gmail.com>
2014-12-23 16:40:48 -0500

    Initial Commit

---
+++ HuntTimehop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HuntTimehop
+//
+//  Created by thomas on 12/23/14.
+//  Copyright (c) 2014 thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
